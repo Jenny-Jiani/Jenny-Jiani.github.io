9e228e970d083760f70fd90a46b63cf889f8e8f9
Jenny <jenny@dynamsoft.com>
2020-08-26 14:22:09 +0800

    删减文件

--- assets/js/ds-jquery-bs.js
+++ assets/js/ds-jquery-bs.js
@@ -147,12 +147,26 @@ $(function(){
 		stopPropagation(e);
 	});	
 	
+	//Image lazy loading
+	$(".lazy").lazyload({
+		effect:"fadeIn",
+		threshold : 100,
+		placeholder:"https://www.dynamsoft.com/assets/images/img-Placeholder-1x1.png"
+		});
+	setTimeout(function(){
+    	$(".lazy-bg").lazyload({
+			effect:"fadeIn",
+			placeholder:"https://www.dynamsoft.com/assets/images/img-Placeholder-1x1.png"
+			//threshold : 500
+		});
+	},1000);
+	
 	//cookieAlert
 	var cookieWarnText = '<div class="container pr"><p>We use cookies to study how our website is being used. You consent to our cookies if you continue to use this website.</p><span class="close-cookieAlert"><i class="fa fa-times close" aria-hidden="true"></i></span></div>';
 	var cookieWarn = $("<div id='cookieAlert'></div>").html(cookieWarnText);
 	$.ajax({
 		type: "GET",
-		url: "../CustomerPortal/CookieHints.ashx",
+		url: "https://www.dynamsoft.com/CustomerPortal/CookieHints.ashx",
 		//dataType: "json",
 		success: function (returnData) {			
             var visits = $.getCookie("cookie-alert")||$.getCookie("DSCookieCompliancyAccepted");
@@ -416,5 +430,3 @@ function scrollToMainContent(){
 		}
 	});
 })(jQuery);
-
-
