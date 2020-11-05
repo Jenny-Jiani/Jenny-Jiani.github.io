d774c00c7a69a7700d7ceb397ee5515c2d0c7583
Jenny <jenny@dynamsoft.com>
2020-11-05 13:10:53 +0800

    add download part in docs

--- assets/js/ds-jquery-bs.js
+++ assets/js/ds-jquery-bs.js
@@ -301,3 +301,14 @@ function scrollToMainContent(){
 		
 	};
 })(jQuery);
+
+
+function getUrlParam(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null) {
+			return unescape(r[2]);
+	} else {
+			return null;
+	}
+}
